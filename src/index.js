8dd09a87ecc5c5227dbf3e2878311d25f3eb1d76
Nikhil <gnikhil556@gmail.com>
2024-01-28 12:41:26 +0530

    build signup functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
+
+const apiRoutes = require('./routes/index');
 
 const app = express();
 
 const prepareAndStartServer = () => {
-    app.listen(3001, ()=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, ()=>{
         console.log(`Server Started on Port: ${PORT}`);
     })
 }   
